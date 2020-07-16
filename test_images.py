c51d9fc973396cb6116a6a73dd96f8b313d29eb9
bongkyo seo <wenry55@gmail.com>
2020-07-16 21:31:21 +0900

    .

--- test_images.py
+++ test_images.py
@@ -84,3 +84,29 @@ for imgp in flists:
     x = np.expand_dims(x, axis=0) /255
     classes = model.predict(x)
     print(imgp, np.argmax(classes[0])==0, max(classes[0]), classes)
+
+video = cv2.VideoCapture(0)
+while True:
+        _, frame = video.read()
+#Convert the captured frame into RGB
+        im = Image.fromarray(frame, 'RGB')
+#Resizing into 224x224 because we trained the model with this image size.
+        im = im.resize((224,224))
+        img_array = image.img_to_array(im)
+        img_array = np.expand_dims(img_array, axis=0) / 255
+        probabilities = model.predict(img_array)# [0]
+        #Calling the predict method on model to predict 'fire' on the image
+        prediction = np.argmax(probabilities)
+        print(prediction)
+        #if prediction is 0, which means there is fire in the frame.
+        #if prediction == 0:
+        #        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        #        print(probabilities[prediction])
+#cv2.imshow("Capturing", frame)
+        sleep(3)
+        key=cv2.waitKey(1)
+        if key == ord('q'):
+                break
+
+video.release()
+cv2.destroyAllWindows()
